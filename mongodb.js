1ea456b757f00db8003e4bb6b612892f9e466bcd
yenilikci <m.melihcelik017@outlook.com>
2021-06-24 18:06:32 +0300

    veri ekleme işlemi gerçekleştirildi, insertOne ile koleksiyona veri eklendi

--- mongodb.js
+++ mongodb.js
@@ -10,10 +10,36 @@ app.get('/',(req,res) => {
     res.send('welcome to mongodb api')
 })
 
+//tüm verileri getir
 app.get('/api/books',(req,res) => {
     database.collection('books').find({}).toArray((err,result) => {
         if(err) throw err
         res.send(result)
+    })
+})
+
+//id'ye göre veri getir
+app.get('/api/books/:id',(req,res) => {
+    database.collection('books').find({id:parseInt(req.params.id)}).toArray((err,result) => {
+        if(err) throw err
+        res.send(result)
+    })
+})
+
+//veri ekle
+app.post('/api/books/addBook',(req,res)=>{
+    let resp = database.collection('books').find({}).sort({id:-1}).limit(1)
+    resp.forEach(obj => {
+        if(obj){
+            let book = {
+                id:obj.id+1,
+                title:req.body.title
+            }
+            database.collection('books').insertOne(book,(err,result) => {
+                if(err) res.status(500).send(err)
+                res.send('Added succesful')
+            })
+        }
     })
 })
 
